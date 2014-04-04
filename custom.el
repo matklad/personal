4e4278b06b67da74e6d17bfab274f8013646b676
matklad <aleksey.kladov@gmail.com>
2014-04-04 14:25:23 +0400

    initial

---
+++ custom.el
@@ -0,0 +1,12 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("de2c46ed1752b0d0423cde9b6401062b67a6a1300c068d5d7f67725adc6c3afb" "51bea7765ddaee2aac2983fac8099ec7d62dff47b708aa3595ad29899e9e9e44" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ personal.el
@@ -0,0 +1,81 @@
+;;; personal.el --- personal stuff
+;;; Commentary:
+
+;;; Code:
+(prelude-require-packages '(tuareg golden-ratio auto-complete ac-nrepl
+                                   base16-theme emmet-mode yasnippet))
+
+(load-theme 'base16-solarized)
+(require 'hl-line)
+(global-hl-line-mode 0)
+
+
+(golden-ratio-mode 1)
+
+(require 'whitespace)
+(setq whitespace-line-column 120)
+
+(require 'yasnippet)
+(setq yas-snippet-dirs
+      '("~/.emacs.d/personal/snippets"))
+(yas-global-mode 1)
+
+
+;; Auto-Complete
+
+(require 'auto-complete)
+(global-auto-complete-mode t)
+(setq ac-delay 0.05)
+
+
+;; Web
+
+(require 'web-mode)
+(require 'prelude-web)
+(require 'emmet-mode)
+
+(add-to-list 'auto-mode-alist '("\\.hbs?\\'" . web-mode))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+
+(setq prelude-web-mode-hook 'my-web-mode-hook)
+(add-hook 'prelude-web-mode-hook 'prelude-web-mode-defaults)
+
+(add-hook 'prelude-web-mode-hook 'emmet-mode)
+(add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2)))
+
+;; OCaml
+(if (= 0 (shell-command "opam"))
+    (do
+        (add-to-list 'load-path
+                     (concat
+                      (replace-regexp-in-string
+                       "\n$" ""
+                       (shell-command-to-string "opam config var share"))
+                      "/emacs/site-lisp"))
+
+        (let* ((opam-prefix
+                (substring (shell-command-to-string "opam config var prefix") 0 -1)))
+          (with-temp-buffer
+            (insert (shell-command-to-string
+                     (concat opam-prefix
+                             "/bin/ocp-edit-mode emacs -load-global-config")))
+            (eval-buffer)))
+
+      (require 'ocp-indent)
+      (require 'ocp-index)
+      (require 'tuareg)
+      (setq ocp-indent-config "with_never=true")
+
+      (autoload 'merlin-mode "merlin" "Merlin mode" t)
+      (add-hook 'tuareg-mode-hook 'merlin-mode)
+      (add-hook 'caml-mode-hook 'merlin-mode)
+      (eval-after-load 'merlin
+        '(progn (define-key merlin-mode-map (kbd "C-<up>") nil)
+                (define-key merlin-mode-map (kbd "C-<down>") nil)))))
+
+;;; personal.el ends here
